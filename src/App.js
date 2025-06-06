3c22d674d46e7b30dd5b8f4584b501b5fa2cb73e
Garvit Hinger <garvithinger24@gmail.com>
2025-06-06 10:23:27 +0530

    improvement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 import NavBar from './components/NavBar.js'
 import ImageSection from './components/ImageSection.js'
 import Description from './components/Description.js'
@@ -15,37 +16,55 @@ import StickyBar from './components/StickyBar.js'
 
 
 
+
+
+  
+
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  useEffect(() => {
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+    
+  window.addEventListener("resize", handleResize);
+
+  // Clean up
+  return () => window.removeEventListener("resize", handleResize);
+}, []);
+
   return (
     <div className="app-wrapper">
-      <NavBar />
-      <div className="content">
-        <div className="left-part">
-          <ImageSection />
-        </div>
-        <div className="right-part">
-          <Description />
-          <hr className="app-line" />
-          <ColorChoice />
-          <hr className="app-line"/>
-          <RingSizeKit />
-          <hr className="app-line"/>
-          <Charger />
-          <hr className="app-line"/>
-          <Trade />
-          <hr className="app-line"/>
-          <Engraving />
-          <hr className="app-line"/>
-          <PowerPlug />
-          <hr className="app-line"/>
-          <Protection />
-          <hr className="app-line"/>
-          <AddToCart />
-          
-        </div>
-      </div>
-      {/* <StickyBar /> */}
+  <NavBar />
+
+  <div className="content">
+    {isMobile && <div className="description-mobile"><Description /></div>}
+
+    <div className="left-part">
+      <ImageSection />
     </div>
+
+    <div className="right-part">
+      {!isMobile && <Description />}
+      <hr className="app-line" />
+      <ColorChoice />
+      <hr className="app-line" />
+      <RingSizeKit />
+      <hr className="app-line" />
+      <Charger />
+      <hr className="app-line" />
+      <Trade />
+      <hr className="app-line" />
+      <Engraving />
+      <hr className="app-line" />
+      <PowerPlug />
+      <hr className="app-line" />
+      <Protection />
+      <hr className="app-line" />
+      <AddToCart />
+    </div>
+  </div>
+</div>
   );
 }
 
